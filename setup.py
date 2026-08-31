0c7dbc7ad40dd37324fc5ef7e25acbf2fb4bb3de
iwahle <imanwahle@gmail.com>
2026-08-31 08:59:33 -0400

    Initial commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cfl_lbm_comp',
+    name='cfl_lbm',
     version='0.0.1',
     packages=find_packages(include=['source', 'source.*'])
 )
